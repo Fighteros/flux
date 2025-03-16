27e6ad6662165def210430c7614af71a7a7748b4
Ahmed M. Abd Elghany <am5419@fayoum.edu.eg>
2025-03-16 07:37:36 +0200

    reapply .gitignore for backend

--- apps/backend/src/auth/strategies/local.straregy.ts
+++ apps/backend/src/auth/strategies/local.straregy.ts
@@ -9,6 +9,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super();
   }
 
+  // local strategy
   async validate(email: string, password: string) {
     const user = await this.authService.validateUser({
       email,
