fb3c76d33930ac6df967270313b6c12180690c86
Ahmed Abd ELGhany <am5419@fayoum.edu.eg>
2025-03-19 01:24:46 +0200

    admin seeder

---
+++ apps/frontend/src/components/posts/Posts.tsx
@@ -0,0 +1,13 @@
+import {Post} from '@lib/types/modelTypes';
+
+type PostProps = {
+    posts: Post[]
+}
+
+const Post = (props: PostProps) => {
+    return (
+        <div></div>
+    );
+}
+
+export default Post;
